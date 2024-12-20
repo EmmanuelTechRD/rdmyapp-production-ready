041d9361e43a6b0decc3254fa330535ee9f2c807
Emmanuel Soto <emmanueltechrd@gmail.com>
2024-12-20 11:19:31 -0400

    [HOTFIX] Settings file updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["rdmyapp-production-ready-14117cded9f8.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
