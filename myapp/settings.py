2ccf20d9374712fa2eeb0774a143d5fc48f066d7
Emmanuel Soto <emmanueltechrd@gmail.com>
2025-01-15 10:08:44 -0400

    [UPDATE] Postgres db as default

--- myapp/settings.py
+++ myapp/settings.py
@@ -89,12 +89,9 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(conn_max_age=600)
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
