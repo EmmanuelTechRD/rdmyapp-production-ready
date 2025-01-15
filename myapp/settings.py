1436b905ca920cd03830c41f1e5fac7562c2f6fb
Emmanuel Soto <emmanueltechrd@gmail.com>
2025-01-15 10:18:40 -0400

    [UPDATE] Blog migrations

--- myapp/settings.py
+++ myapp/settings.py
@@ -89,9 +89,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        dj_database_url.config(conn_max_age=600)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
