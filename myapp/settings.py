09f7dce058003ce71ff7d1b828324e21ec52405c
Emmanuel Soto <emmanueltechrd@gmail.com>
2025-01-16 13:32:29 -0400

    [ADD] AWS S3 Bucket Service

--- myapp/settings.py
+++ myapp/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     # Local apps
     'blog',
     
+    # Third-party apps
+    'storages',
+    
     # IMPORTANT: django_cleanup should be placed last in INSTALLED_APPS
     'django_cleanup.apps.CleanupConfig',
 ]
@@ -146,6 +149,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+
+# S3 Buckets Config
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
